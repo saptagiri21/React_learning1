79a677e57e45766d8d64ff83ba5356817c6d1c06
Saptagiri <saptagiri.gandla@gmail.com>
2026-01-07 20:35:49 +0530

    without state

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,1 +1,18 @@
+function Click() {
+  let count = 0;
+
+  function onClick() {
+    count = count + 1;
+  }
+  return (
+    <>
+      <p>{count}/</p>
+      <button onClick={onClick}>increase</button>
+    </>
+  );
+}
+
+function App() {
+  <Click />;
+}
 export default App;
