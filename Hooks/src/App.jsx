d9040106d7c2f3531d1079aa6332066bcffb447d
Saptagiri <saptagiri.gandla@gmail.com>
2026-01-07 22:57:38 +0530

    usig useEffect JS way

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Click() {
   let count = 0;
@@ -17,14 +17,19 @@ function Click() {
 function OnClick() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.getElementById("count").innerText = "increased";
+  }, [count]);
+
   let click = () => {
-    setCount(count + 1);
+    setCount(count + 1); // state which helps us to remember data in react
   };
   return (
     <>
       <h1>counter</h1>
       <p>{count}</p>
       <button onClick={click}>Increase</button>
+      <p id="count"></p>
     </>
   );
 }
